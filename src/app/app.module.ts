1ff504954780f66bc7a9f48438e05c351a5444be
paolaj <paoj398@gmail.com>
2021-03-17 18:56:38 -0430

    avances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+import { RouterModule, Routes} from '@angular/router';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{ EquipoService } from './equipo.service';
+
 import { AppComponent } from './app.component';
 import { AppPaolaComponent } from './app-paola/app-paola.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes=[
+  { path:'header', component: HeaderComponent },
+  { path:'inicio', component: InicioComponent },
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppPaolaComponent
+    AppPaolaComponent,
+    HeaderComponent,
+    InicioComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    EquipoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
